2522922a7d394c2c771a01444bec4bb7c64e05bb
wjh <wjh2012@naver.com>
2026-01-07 19:18:47 +0900

    cleanup

--- apps/doc-flow-api/src/app.module.ts
+++ apps/doc-flow-api/src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './infra/database/database.module';
 import { CacheModule } from '@nestjs/cache-manager';
 import { BullModule } from '@nestjs/bullmq';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { TaskService } from './task/task.service';
 import { TaskController } from './task/task.controller';
 import { DocFlowQueueModule } from './infra/message/doc-flow-queue.module';
@@ -13,16 +10,6 @@ import { DocFlowQueueModule } from './infra/message/doc-flow-queue.module';
   imports: [
     DatabaseModule,
     CacheModule.register(),
-    ClientsModule.register([
-      {
-        name: 'hello_tcp',
-        transport: Transport.TCP,
-        options: {
-          host: 'localhost',
-          port: 4001,
-        },
-      },
-    ]),
     BullModule.forRoot({
       connection: {
         host: 'localhost',
@@ -31,7 +18,7 @@ import { DocFlowQueueModule } from './infra/message/doc-flow-queue.module';
     }),
     DocFlowQueueModule,
   ],
-  controllers: [AppController, TaskController],
-  providers: [AppService, TaskService],
+  controllers: [TaskController],
+  providers: [TaskService],
 })
 export class AppModule {}
