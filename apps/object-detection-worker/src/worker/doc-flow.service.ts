37786874a75b867b84f9c9c89475eb0349efdc0d
wjh <wjh2012@naver.com>
2026-01-08 19:21:06 +0900

    refactor: worker 공통모듈 분리

--- apps/object-detection-worker/src/worker/doc-flow.service.ts
+++ apps/object-detection-worker/src/worker/doc-flow.service.ts
@@ -1,34 +1,24 @@
 import { Injectable, Inject } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
 import { CreateDocJobPayload } from '@app/shared';
-import { KYSELY_DB, WorkerDatabase, TaskStatus } from '@app/database';
+import { BaseWorkerService } from '@app/worker-common';
+import { KYSELY_DB, WorkerDatabase } from '@app/database';
 import { Kysely } from 'kysely';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
-export class DocFlowService {
+export class DocFlowService extends BaseWorkerService {
   constructor(
-    @Inject(KYSELY_DB) private readonly db: Kysely<WorkerDatabase>,
-    @Inject('TASK_SERVICE') private readonly client: ClientProxy,
-  ) {}
+    @Inject(KYSELY_DB) protected readonly db: Kysely<WorkerDatabase>,
+    @Inject('TASK_SERVICE') protected readonly client: ClientProxy,
+  ) {
+    super(db, client);
+  }
 
-  async createDocument(data: CreateDocJobPayload) {
+  async processTask(data: CreateDocJobPayload) {
     console.log('Processing document...', data);
 
     await new Promise((resolve) => setTimeout(resolve, 1000));
 
     await this.updateJobStatus(data.docId, 'SUCCESS');
-  }
-
-  async updateJobStatus(id: string, status: TaskStatus) {
-    const updatedTask = await this.db
-      .updateTable('task')
-      .set({ status })
-      .where('id', '=', id)
-      .returningAll()
-      .executeTakeFirst();
-
-    if (updatedTask) {
-      this.client.emit('task_status_updates', updatedTask);
-    }
   }
 }
--- apps/ocr-worker/src/worker/doc-flow.worker.ts
+++ apps/ocr-worker/src/worker/doc-flow.worker.ts
@@ -10,7 +10,7 @@ export class DocFlowWorker extends WorkerHost {
   }
 
   async process(job: Job<CreateDocJobPayload>) {
-    await this.docFlowService.createDocument(job.data);
+    await this.docFlowService.processTask(job.data);
   }
 
   @OnWorkerEvent('active')
